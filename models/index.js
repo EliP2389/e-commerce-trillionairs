5a62f1a7065291419bbda64db04a08a745242fbf
Eli Pagan <mbk8689@gmail.com>
2022-01-06 16:26:08 -0500

    added products belongsToMany tags

--- models/index.js
+++ models/index.js
@@ -15,6 +15,11 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
 
