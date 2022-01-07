c4087d697d853272e4da0b81e5fa7f11983b3c74
Eli Pagan <mbk8689@gmail.com>
2022-01-07 13:39:09 -0500

    added findOne route to Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
   // find all products
   Product.findAll({
     attributes: [
+      'id',
       'product_name',
       'price',
       'stock',
@@ -42,7 +43,47 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'category_name'
+        ]
+      },
+      {
+        model: Tag,
+        attributes: [
+          'id',
+          'tag_name'
+        ],
+        through: ProductTag,
+        as: 'product_tags',
+      }
+    ]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
